d53509583b17a972acc12c27922fec6ff68874b1
“shivams112” <shivams.sharma12@gmail.com>
2020-06-08 17:03:25 +0530

    getuser route, update purchases done

--- projbackend/routes/user.js
+++ projbackend/routes/user.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const {findUserById, getUser} = require("../controllers/user");
-const {isSignedIn, isAuthenticated, isAdmin} = require("../controllers/auth");
+const { findUserById, getUser, updateUser, userPurchaseList} = require("../controllers/user");
+const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 
 router.param("userId", findUserById);
 
-router.get("/user/:userId", isSignedIn, isAuthenticated, getUser)
+router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
+router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser);
+router.get("orders/user/:userId", isSignedIn, isAuthenticated, userPurchaseList);
 
 module.exports = router;
