7ad2a499f031ddfa8f99393db4f92ecf02476395
“shivams112” <shivams.sharma12@gmail.com>
2020-08-01 17:44:09 +0530

    make it better

--- app.js
+++ app.js
@@ -6,20 +6,21 @@ const app = express();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const path = require("path");
 
 //My Routes
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
-const orderRoutes = require("./routes/order")
+const orderRoutes = require("./routes/order");
 
 //PORT Number
 const port = process.env.PORT || 4000;
 
 //DB Connection
 mongoose
-  .connect(process.env.MONGOLAB_URI, {
+  .connect(process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/store', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -41,8 +42,14 @@ app.use("/api", productRoutes);
 app.use("/api", orderRoutes);
 
 //chacking for production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('projfrontend/build'))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("projfrontend/build/"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "projfrontend", "build", "index.html")
+    );
+  });
 }
 
 //Starting server
